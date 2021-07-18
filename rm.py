4daadc1f4deb5cd8664d3f3dca6004b4688c8075
Er1c-Chen <ec-atw@outlook.com>
2021-07-18 11:57:50 +0800

    update

--- rm.py
+++ rm.py
@@ -1,26 +1,11 @@
 import numpy as np
 import cv2
-from sklearn.neighbors import KNeighborsClassifier
+import copy
 
+kernel = np.ones((5, 5), np.uint8)
 
-winname = 'hi'
-winname1 = 'hii'
-kernel = np.ones((5,5),np.uint8)
-
-cv2.namedWindow(winname, 0)
-cv2.namedWindow(winname1, 0)
-
-img = cv2.imread('img.jpg')
 cap = cv2.VideoCapture()
 cap.open('red.avi')
-
-# clf = KNeighborsClassifier()
-
-class distanceClassifier():
-    def cls(self, x):
-        cv2.imread('true.jpg')
-
-        np.ndarray.flatten(x)
 
 while cap.isOpened():
     ret, img = cap.read()
@@ -30,15 +15,12 @@ while cap.isOpened():
     thresh, ret = cv2.threshold(subt, 95, 255, cv2.THRESH_BINARY)
     dilation = cv2.dilate(ret, kernel, iterations=1)
     prepro = cv2.erode(dilation, kernel, iterations=1)
-    '''
+    flood = copy.deepcopy(prepro)
     h, w = prepro.shape[:2]
     mask = np.zeros([h + 2, w + 2], np.uint8)
-    cv2.floodFill(prepro, mask, (0, 0), (255, 255, 255), flags=cv2.FLOODFILL_FIXED_RANGE)
-    '''
-    # cv2.imshow(winname1, prepro)
+    cv2.floodFill(flood, mask, (0, 0), (255, 255, 255), flags=cv2.FLOODFILL_FIXED_RANGE)
 
     contours, hire = cv2.findContours(prepro, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-
     if len(contours) > 0:
         for i, cnt in enumerate(contours):
             rect = cv2.minAreaRect(cnt)
@@ -52,22 +34,45 @@ while cap.isOpened():
                 right_point_x = int(np.max(box[:, 0]))
                 top_point_y = int(np.min(box[:, 1]))
                 bottom_point_y = int(np.max(box[:, 1]))
-                cropImg = prepro[top_point_y:bottom_point_y, left_point_x:right_point_x]
+                cropImg = flood[top_point_y:bottom_point_y, left_point_x:right_point_x]
 
                 src = np.float32([box[0], box[1], box[2], box[3]])
-                dst = np.float32([[0, height], [0, 0], [width, 0],  [width, height]])
+                dst = np.float32([[0, height], [0, 0], [width, 0], [width, height]])
                 trans = cv2.getPerspectiveTransform(src, dst)
-                afterTrans = cv2.warpPerspective(prepro, trans, (int(width), int(height)))
+                afterTrans = cv2.warpPerspective(flood, trans, (int(width), int(height)))
 
-                if height < width:
-                    height, width = width, height
-                    trans_img = cv2.transpose(afterTrans)
-                    target = cv2.flip(trans_img, 1)
-                # label =
-                cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+                whitePx = np.count_nonzero(afterTrans)
+                blackPx = area - whitePx
+                ratio = blackPx / whitePx
 
+                if ratio > 0.8:
+                    cv2.drawContours(img, [box], 0, (255, 0, 0), 2)
+                elif ratio < 0.3:
+                    rec = cv2.minAreaRect(contours[hire[0][i][2]])
+                    boxx = cv2.boxPoints(rec)
+                    boxx = np.int0(boxx)
+                    wid = rec[1][0]
+                    hei = rec[1][1]
+                    rat = wid / height
+                    print(rat)
+                    if 0.25 < rat < 0.65:
+                        center = (int(rec[0][0]), int(rec[0][1]))
+                        print(center)
+                        cv2.circle(img, center, 3, (0, 255, 0), -1)
 
-    cv2.imshow(winname, img)
-    # cv2.imshow(winname1, ret)
+                    cv2.drawContours(img, [boxx], 0, (0, 255, 0), 2)
+                '''
+                    bla = cv2.boundingRect(afterTrans)
+                    center = (int(bla[0]+0.5*bla[2]), int(bla[1]+0.5*bla[3]))
+                    cv2.circle(img, center, 3, (0, 255, 0), -1)
+                    con, hir = cv2.findContours(flood, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+                    if len(con) > 0:
+                        for t, cntr in enumerate(con):
+                            rec = cv2.minAreaRect(cntr)
+                            
+                    cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+                    '''
+
+    cv2.imshow('test', img)
     cv2.waitKey(3)
 cv2.destroyAllWindows()
